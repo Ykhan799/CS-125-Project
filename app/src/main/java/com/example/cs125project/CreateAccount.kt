5b21e27677a495ac008966406e4d97758fc115a0
Emily Doan <emmiedoan@gmail.com>
2023-02-22 21:38:35 -0800

    added home page files

---
+++ app/src/main/java/com/example/cs125project/CreateAccount.kt
@@ -0,0 +1,11 @@
+package com.example.cs125project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
---
+++ app/src/main/java/com/example/cs125project/HomePage.kt
@@ -0,0 +1,11 @@
+package com.example.cs125project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
