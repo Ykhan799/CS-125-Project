466c147d8da62611f43724868a5f5be3a3046cbf
Emily Doan <emmiedoan@gmail.com>
2023-02-23 01:05:40 -0800

    really basic about you so far

---
+++ app/src/main/java/com/example/cs125project/AboutYou.kt
@@ -0,0 +1,11 @@
+package com.example.cs125project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutYou : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_you)
+    }
+}
