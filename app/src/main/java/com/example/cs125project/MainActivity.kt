e7f16bbd4f1359adbc613060969460f5bbc397ba
ryan cruz <rncruz1@uci.edu>
2023-02-24 12:52:49 -0800

    added the bottom navigation menu

--- app/src/main/java/com/example/cs125project/MainActivity.kt
+++ app/src/main/java/com/example/cs125project/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.cs125project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.cs125project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+
+
     }
 }
