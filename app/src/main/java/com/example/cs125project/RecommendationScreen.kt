7a96d2d01ea166cd61cfc7dfbeefd4ad05116f4f
ryan cruz <rncruz1@uci.edu>
2023-03-12 17:04:36 -0700

    added requesting for API ninjas exercise API

--- app/src/main/java/com/example/cs125project/RecommendationScreen.kt
+++ app/src/main/java/com/example/cs125project/RecommendationScreen.kt
@@ -4,6 +4,8 @@ import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_recommendations.*
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -16,9 +18,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://api.api-ninjas.com/"
 class RecommendationScreen : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommendations)
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getPost()
+        viewModel.myReponse.observe(this, Observer { response ->
+            if(response.isSuccessful){
+                response.body()?.forEach{
+                    Log.d("Response", it.name)
+                    Log.d("Response","----------------------")
+                }
+            }else{
+                Log.d("Response", response.code().toString())
+            }
+        })
 
     }
 
